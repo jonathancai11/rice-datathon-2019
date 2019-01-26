25ff218c63d6c505eabd39d0de08be77addec61f
zoinx <bakitybacon@gmail.com>
2019-01-26 00:20:11 -0600

    added json data and processed week long data

---
+++ parsing/daemon.py
@@ -0,0 +1,6 @@
+import time
+import os
+
+while True:
+    os.system('python2 processor.py')
+    time.sleep(120)
--- parsing/processor.py
+++ parsing/processor.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-emotions = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust']
+from pylab import rcParams
+rcParams['figure.figsize'] = 30, 10
+
+emotions = ['fear', 'anger',  'disgust', 'sadness', 'surprise', 'anticipation', 'joy', 'trust']
+emotion_colors = ['black', 'red']
 emotion_df = pd.read_csv('lexicons_compiled.csv')
 
 def emotion_list(emotion):
@@ -14,7 +20,7 @@ def calculate_emotion_score(text, emotion):
     return sum(emotion_dict[emotion].isin(words))
     
 
-tweets = pd.read_csv('../twitter/data/2019-01-23:2019-01-24.csv')
+tweets = pd.read_csv('../twitter/data/week-full.csv')
 
 tweets.columns = ['date','text','cityst','country','platform']
 
@@ -23,8 +29,21 @@ for emotion in emotions:
 
 tweets['date'] = pd.to_datetime(tweets['date'])
 tweets['minute'] = tweets['date'].apply(lambda date: date.minute)
+tweets['second'] = tweets['date'].apply(lambda date: date.second)
+tweets['day'] = tweets['date'].apply(lambda date: date.day)
 
-print(tweets.groupby('minute').agg('sum').to_csv())
+grouper = tweets.groupby('day').agg('sum')
+print(grouper.to_json())
+grouper = grouper.reset_index()
 
-#print(tweets.to_csv())
+#print(grouper[emotions].values)
+
+#second_grouper = tweets.groupby('second').agg('sum').reset_index()
+#print(second_grouper['second'].values)
+#print(second_grouper[emotions].values)
+
+plt.stackplot(grouper['day'].values, grouper[emotions].values.T, labels=emotions)
+plt.title("One Week")
+plt.legend(loc='best')
+plt.show()
 
